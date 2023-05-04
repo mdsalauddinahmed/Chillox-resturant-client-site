7d3597a2ca2ce4702881d6ff61a801a9f63b5784
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-05-04 21:18:51 +0600

    homepage responsive done

--- src/Components/ListOfChefs/ListOfChefs.jsx
+++ src/Components/ListOfChefs/ListOfChefs.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import SingleChef from './SingleChef';
+import chef from './chefs.css'
+import { Container } from 'react-bootstrap';
 
 const ListOfChefs = () => {
 
     const[allChef,setAllChef]=useState([])
+    const [showAll, setShowAll] = useState(false);
 
       useEffect(()=>{
         fetch(`https://bonolota-server-mdsalauddinahmed.vercel.app`)
@@ -13,19 +16,42 @@ const ListOfChefs = () => {
       },[])
       console.log(allChef)
     return (
-        <div className=' '>
+        <Container className=' '>
           <h2 className='text-center my-5'>Let's familiar Our Chefs</h2>
-
-      {
+<div className=''>
+  
+{ showAll?
        allChef.map(data=> 
           
         <SingleChef data={data} key={data.id}>
 
-        </SingleChef>)
+        </SingleChef>):allChef.slice(0,6).map(data=><SingleChef data={data}></SingleChef>)
       } 
 
-            
-        </div>
+</div>
+
+    
+<div className='size   my-3'>
+   {!showAll && (
+        <button
+          onClick={() => setShowAll(true)}
+          className="   border-0 rounded   "
+        >
+          Show All
+        </button>
+      )}
+       </div>
+   </Container>
+
+
+
+
+
+
+       
+   
+
+
     );
 };
 
