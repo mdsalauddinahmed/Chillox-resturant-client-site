a9c0642e90f4b6b1720aee18e74b511dc104860a
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-05-04 19:44:22 +0600

    react app environmet variable hide

--- src/Firebase/Firebase.config.jsx
+++ src/Firebase/Firebase.config.jsx
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
  
 const firebaseConfig = {
-  apiKey: "AIzaSyAi24fHOnIgdpEMX5ysYTl6aPJOmIUGeeE",
-  authDomain: "assignment-10-2bc1e.firebaseapp.com",
-  projectId: "assignment-10-2bc1e",
-  storageBucket: "assignment-10-2bc1e.appspot.com",
-  messagingSenderId: "601879019819",
-  appId: "1:601879019819:web:9285daadac7b1ab58d856e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env. VITE_AUTHDOMAIN, 
+  projectId:  import.meta.env. VITE_PROJECTID,
+  StorageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:  import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
